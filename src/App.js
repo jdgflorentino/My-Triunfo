65f50289adb6c580abec87a0362c5d9a31191403
Jéssica Florentino <jdgflorentino@gmail.com>
2022-02-24 09:25:31 -0300

    Requisito 2.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React from 'react';
-import Form from './Form';
+import Form from './components/Form';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const { state: {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled }, onInputChange } = this;
+
     return (
       <div>
-        <Form />
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ onInputChange }
+        />
       </div>
     );
   }
